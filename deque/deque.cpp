f8c60013c0b2957278ced9b9e64b1d0dd6ad37f3
PaSawi <sawicki.patryk@gmail.com>
2023-01-28 15:57:08 +0100

    deque works

--- deque/deque.cpp
+++ deque/deque.cpp
@@ -1,3 +1,23 @@
+#include <cstdlib>
+#include <ctime>
 #include <deque>
 #include <iostream>
-int main() {}
+using namespace std;
+
+void dispDeque(deque<int> deq) {
+  for (auto &el : deq) {
+    cout << el << " ";
+  }
+  cout << "\n";
+}
+
+int main() {
+  deque<int> d;
+  srand(static_cast<int>(time(nullptr)));
+  for (int i = 0; i < 5; i++) {
+    int randV = (rand() % 50);
+    d.emplace_back(randV);
+  }
+  d.emplace_back(3);
+  dispDeque(d);
+}
