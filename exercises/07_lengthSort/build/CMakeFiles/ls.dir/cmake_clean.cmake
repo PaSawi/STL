b37fcd2f68e21dc6b998a0ecf3fd52d54c385002
PaSawi <sawicki.patryk@gmail.com>
2023-05-02 16:23:00 +0200

    lengthSort works

---
+++ exercises/07_lengthSort/build/CMakeFiles/ls.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ls.dir/ls.cpp.o"
+  "CMakeFiles/ls.dir/ls.cpp.o.d"
+  "ls"
+  "ls.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- exercises/07_lengthSort/build/cmake_install.cmake
+++ exercises/07_lengthSort/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/patryks/Workspace/STL/exercises/03_vector
+# Install script for directory: /home/patryks/Workspace/STL/exercises/07_lengthSort
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/patryks/Workspace/STL/exercises/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/patryks/Workspace/STL/exercises/07_lengthSort/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
