b767e398eaace82457cb600243b1fcc990741532
PaSawi <sawicki.patryk@gmail.com>
2023-05-28 17:42:05 +0200

    clean repo

---
+++ exercises/06_removeVowels/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/patryks/Workspace/STL/exercises/06_removeVowels
+# Build directory: /home/patryks/Workspace/STL/exercises/06_removeVowels/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/patryks/Workspace/STL/exercises/06_removeVowels/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
