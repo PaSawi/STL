98005ded02a3ab235042bf9203c6b71b27644989
PaSawi <sawicki.patryk@gmail.com>
2023-06-17 17:42:54 +0200

    add template printCollection

---
+++ printCollection.cpp
@@ -0,0 +1,11 @@
+//print collection
+
+#include <iostream>
+
+template <typename Container>
+void printCollection(const Container& collection) {
+    for (const auto& element : collection) {
+        std::cout << element << ' ';
+    }
+    std::cout << '\n';
+}
