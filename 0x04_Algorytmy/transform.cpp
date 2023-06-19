98669b667250e1b6fea4246bcc284e424bbc8442
Patryk Sawicki <patryk.sawicki@polon-alfa.pl>
2023-06-19 10:28:32 +0200

    add generate.cpp,  add ex_01

--- 0x04_Algorytmy/transform.cpp
+++ 0x04_Algorytmy/transform.cpp
@@ -2,20 +2,20 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include "printCollection.cpp"
 
-int main(){
-    std::vector<int> vec {1,2,3,4,5,6,7,8};
-    std::list<int> list {10,20,30,40,50,60,70,80};
-    std::transform(begin(vec),
-                   end(vec),
-                   begin(list),
-                   end(list),
-                   [](auto first, auto second){
-                        return first + second;
-                   });
-                   
-
-
-
-
+int main()
+{
+     std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8};
+     std::list<int> list{10, 20, 30, 40, 50, 60, 70, 80};
+     std::transform(begin(vec),   //begin first container
+                    end(vec),     //end first container
+                    begin(list),  //begin second container
+                    begin(vec),   //output container
+                    [](auto first, auto second)
+                    {
+                         return first + second;
+                    });
+     printCollection(vec);
+     printCollection(list);  
 }
