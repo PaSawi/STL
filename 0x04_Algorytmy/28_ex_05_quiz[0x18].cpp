df56a94cf78e2e4f331b472c321b7883a9f79331
PaSawi <sawicki.patryk@gmail.com>
2023-06-28 21:41:05 +0200

    add algorithm quiz

--- 0x04_Algorytmy/28_ex_05_quiz[0x18].cpp
+++ 0x04_Algorytmy/28_ex_05_quiz[0x18].cpp
@@ -1,26 +1,37 @@
 #include <algorithm>
+#include <array>
 #include <execution>
 #include <iostream>
 #include <iterator>
 #include <list>
 #include <numeric>
 #include <vector>
+#include <ranges>
 
 int main() {
     std::vector<int> vec1{1, 2, 3, 4, 5, 6, 7, 8};
     std::vector<int> vec2{10, 20, 30, 40, 50, 60, 70, 80};
-    std::vector<int> result;
+    std::vector<std::array<int,2>> result;
     // 1. połączyć 2 kontenery naprzemiennie?
-    auto size = std::max(vec1.size(), vec2.size());
+    result.reserve(vec1.size() + vec2.size());
+    std::ranges::transform(vec1, vec2, std::back_inserter(result), [](auto a, auto b){
+        return std::array<int,2> {a,b} ;
+    } );
+   
+    std::cout <<'\n';
+    for(const auto& arr : result) {
+        std::cout << arr[0] << " " << arr[1] << " "; // << std::endl;
+    }
+    std::cout <<'\n';
+    
+   /* auto size = std::max(vec1.size(), vec2.size());
     for( auto i = 0; i < size; ++i ) {
         if (i < vec1.size())
             result.push_back(vec1[i]);
         if (i < vec2.size())
             result.push_back(vec2[i]);
-    }
-    std::cout <<'\n';
-    std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout <<'\n';
+    std::ranges::copy(result, std::ostream_iterator<int>(std::cout, " "));
+    }*/
 
         return 0;
 }
