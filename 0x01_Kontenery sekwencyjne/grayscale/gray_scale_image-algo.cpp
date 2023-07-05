1d7fd36c8f4287d3956bfad5197db423dcd59df0
Patryk Sawicki <patryk.sawicki@polon-alfa.pl>
2023-07-05 12:37:50 +0200

    gray_scale_image-algo.cpp cdn .....

--- 0x01_Kontenery sekwencyjne/grayscale/gray_scale_image-algo.cpp
+++ 0x01_Kontenery sekwencyjne/grayscale/gray_scale_image-algo.cpp
@@ -3,15 +3,68 @@
 #include <iostream>
 #include <iterator>
 #include <ranges>
-#include <utility>  //std::pair
+#include <utility> //std::pair
 #include <vector>
 
 constexpr size_t width = 32;
 constexpr size_t height = 32;
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, height>, width>& bitmap)
+{
+    std::vector<std::pair<uint8_t, uint8_t>> vec_c;
+    vec_c.reserve(width * height);
+    int counter = 0;
+    auto last_color = bitmap[0][0];
+    for (const auto& row : bitmap) {
+        std::transform(row.begin(), row.end(), std::back_inserter(vec_c),
+            [&counter, row, &last_color](uint8_t value) mutable {
+                if (value == last_color) {
+                    counter++;
+                    return std::pair<uint8_t, uint8_t>(value, counter);
+                }
+                else {
+                    counter = 1; 
+                    last_color = value;
+                    return std::pair<uint8_t, uint8_t>(value, counter); 
+                }
+            });
+    }
+    return vec_c;
+
+    /*   for (const auto& row : bitmap) {
+           std::transform(row.begin(), row.end(), std::back_inserter(vec_c),
+                          [](uint8_t value) {
+                              return std::pair<uint8_t, uint8_t>(value , 0);
+                          });
+       }
+
+       return vec_c;
+       */
+}
+
+/*bitmap = { { { 0, 0, 0, 1, 1, 2, 3, 0, 0, 0 },
+                     { 0, 0, 4, 4, 4, 1, 1, 1, 1, 1 },
+                     { 2, 2, 2, 2, 2, 1, 2, 2, 2, 2 } } };*/
+// vec_c = {0, 4}, {1, 2}, {2, 1}, {3, 1}, {0, 27}, {4, 3}, {1, 5}, {0, 22}, {2, 5}, {1, 1}, {2, 4}, {0, 182},
+
+int main()
+{
+    std::array<std::array<uint8_t, height>, width>
+        bitmap = { { { 0, 0, 0, 1, 1, 2, 3, 0, 0, 0 },
+            { 0, 0, 4, 4, 4, 1, 1, 1, 1, 1 },
+            { 2, 2, 2, 2, 2, 1, 2, 2, 2, 2 } } };
+    std::vector<std::pair<uint8_t, uint8_t>> vec_c = compressGrayscale(bitmap);
+    std::ranges::for_each(vec_c, [](const std::pair<uint8_t, uint8_t>& p) {
+        std::cout << "{" << static_cast<int>(p.first) << ", " << static_cast<int>(p.second) << "}, ";
+    });
+    std::cout << std::endl;
+    return 0;
+}
+
 /*
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, height>, width>& bitmap)
 {
-    std::vector<std::pair<uint8_t, uint8_t>> compressed;
+    std::vector<std::pair<uint8_t, uint8_t>> vec_c;
     uint8_t last_color = bitmap[0][0];
     uint8_t count = 1;
     for (int row = 0; row < width; row++) {
@@ -19,43 +72,12 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
             if (bitmap[row][col] == last_color) {
                 count++;
             } else {
-                compressed.push_back(std::make_pair(last_color, count));
+                vec_c.push_back(std::make_pair(last_color, count));
                 last_color = bitmap[row][col];
                 count = 1;
             }
         }
     }
-    compressed.push_back(std::make_pair(last_color, count));
-    return compressed;
+    vec_c.push_back(std::make_pair(last_color, count));
+    return vec_c;
 }*/
-
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, height>, width>& bitmap)
-{
-    std::vector<std::pair<uint8_t, uint8_t>> compressed;
-    compressed.reserve(width * height);
-
-    /*auto transformFunction = [&](uint8_t value) {
-        return std::pair<uint8_t, uint8_t>(value, value);
-    };
-
-    std::transform(bitmap.begin(), bitmap.end(), std::back_inserter(compressed),
-        [](const auto& row){
-            return std::transform(row.begin(), row.end(), row.begin(), transformFunction);
-        });*/
-
-    return compressed;
-}
-
-int main()
-{
-    std::array<std::array<uint8_t, height>, width>
-        bitmap = { { { 0, 0, 0, 1, 1, 2, 3, 0, 0, 0 },
-                     { 0, 0, 4, 4, 4, 1, 1, 1, 1, 1 },
-                     { 2, 2, 2, 2, 2, 1, 2, 2, 2, 2 } } };
-    std::vector<std::pair<uint8_t, uint8_t>> compressed = compressGrayscale(bitmap);
-    std::ranges::for_each(compressed, [](const std::pair<uint8_t, uint8_t>& p) {
-        std::cout << "{" << static_cast<int>(p.first) << ", " << static_cast<int>(p.second) << "}, ";
-    });
-    std::cout << std::endl;
-    return 0;
-}
