955d91cd726814ea7062c29dfdbc908bec4a2f4c
PaSawi <sawicki.patryk@gmail.com>
2023-07-10 21:05:11 +0200

    bug fix new row in gray.cpp

--- 0x01_Kontenery sekwencyjne/grayscale/gray.cpp
+++ 0x01_Kontenery sekwencyjne/grayscale/gray.cpp
@@ -3,24 +3,29 @@
 #include <utility>
 #include <vector>
 
-constexpr size_t width = 32;
-constexpr size_t height = 32;
+constexpr size_t width = 3;
+constexpr size_t height = 10;
+
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, height>, width>& bitmap)
 {
     std::vector<std::pair<uint8_t, uint8_t>> compressed;
     uint8_t last_color = bitmap[0][0];
     uint8_t count = 1;
+    int temp = 0;
+
     for (int row = 0; row < width; row++) {
         for (int col = 0; col < height; col++) {
-            if (bitmap[row][col] == last_color) {
+            if (bitmap[row][col] == last_color && temp == 0) {
                 count++;
             } else {
                 compressed.push_back(std::make_pair(last_color, count));
                 last_color = bitmap[row][col];
                 count = 1;
+                temp = 0;
             }
         }
+        temp = 1;
     }
     compressed.push_back(std::make_pair(last_color, count));
     return compressed;
